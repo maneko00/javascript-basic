d6fc62f1a4b07281bf365774c29d677314144e49
initial commit <>
2021-08-01 19:50:52 +0900

    varとletのスコープの違い

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const testScope = (scope) => {
+    if ( scope === 'function' ) {
+        var functionScope = '関数スコープ内ならどこからでも参照できます'
+    } else if ( scope === 'block' ) {
+        let blockScope = 'ブロックスコープ内でしか参照できません'
+        console.log(blockScope)
+    }
+    console.log(functionScope)
+}
+
+testScope('block');
